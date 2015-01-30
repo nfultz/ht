f091e4b42e6b2055e42e168bc0df648ecbb54af9
njf2 <njf2@sd699.zestcash.org>
2015-01-30 00:22:58 +0000

    Fixing for R CMD Check

--- R/ht.R
+++ R/ht.R
@@ -4,10 +4,12 @@
 #' primarily for teaching functions and S3 for R programmers.
 #' 
 #' \code{ht} is an S3 class that extends\code{environment}, and additionally provides \code{[} and \code{[<-}.
+#' It can use arbitrary R objects as keys and values.
 #' 
 #' Currently, default options are assummed for \code{digest}and hash collisions are not dealt with at all.
 #' 
 #' @name ht
+#' @seealso digest
 #' @import digest
 #' @examples
 #'  x <- ht();
@@ -15,8 +17,8 @@
 #'  x[1:2] <- 3:4
 #'  x[1]
 #'  x[1:2]
-#'  x[[digest(1:2)]]
 #'  mget(ls(x),x)
+#'  if(require(digest)) x[[digest(1:2)]]
 NULL
 
 #' @export
@@ -25,14 +27,15 @@ ht <- function(){
   `class<-`(new.env(parent = emptyenv()) , "ht");
 }
 
-#' @param index  The object to use as a key
+#' @param x an \code{ht} object
+#' @param index  A key object
 #' @export
 #' @rdname ht
 `[.ht` <- function(x, index) {
   x[[digest(index)]]$value;
 }
 
-#' @param value The value of the hash table
+#' @param value A value object
 #' @export
 #' @rdname ht
 `[<-.ht` <- function(x, index, value) {
